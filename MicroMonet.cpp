2531defb12fc18de6247a687aff175897ae17ee9
Holger Pirk <holger@pirk.name>
2019-02-01 08:39:56 +0000

    fixed typo

--- MicroMonet.cpp
+++ MicroMonet.cpp
@@ -13,22 +13,23 @@ static void selection(benchmark::State& state) {
     input[i] = (i * 12343) % 499;
 
   for(auto _ : state) {
+		// your code goes here
   }
 	benchmark::DoNotOptimize(input);
 	delete[] input;
 }
 
 BENCHMARK(selection)
-    ->Args(1 * 50)
-    ->Args(2 * 50)
-    ->Args(3 * 50)
-    ->Args(4 * 50)
-    ->Args(5 * 50)
-    ->Args(6 * 50)
-    ->Args(7 * 50)
-    ->Args(8 * 50)
-    ->Args(9 * 50)
-    ->Args(10 * 50)
-    ->Args(11 * 50);
+    ->Arg(1 * 50)
+    ->Arg(2 * 50)
+    ->Arg(3 * 50)
+    ->Arg(4 * 50)
+    ->Arg(5 * 50)
+    ->Arg(6 * 50)
+    ->Arg(7 * 50)
+    ->Arg(8 * 50)
+    ->Arg(9 * 50)
+    ->Arg(10 * 50)
+    ->Arg(11 * 50);
 
 BENCHMARK_MAIN();
